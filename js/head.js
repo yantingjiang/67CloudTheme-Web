22eb0c41e3a82133d5823e768e3231c9fe3d15d7
yantingjiang <3353852279@qq.com>
2022-07-01 20:40:54 +0800

    首页 宝塔页面

--- js/head.js
+++ js/head.js
@@ -3,3 +3,15 @@ $('.head .sub')
 	.on('click', function () {
 		$('.sub', this).slideToggle(200);
 	});
+$('.head .menu').on('click', function () {
+	if ($('.nav').css('left') == '0px') {
+		$('.nav').css('left', '-50%');
+	} else {
+		$('.nav').css('left', '0%');
+	}
+});
+$(window).on('resize', function () {
+	if (window.innerWidth >= 992) {
+		$('.nav').attr('style', '');
+	}
+});
